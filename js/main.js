200cafc7c9bcbf00ed5a6a5c0ec5e43d7aa39e7d
ADAMA1010 <147938653+ADAMA1010@users.noreply.github.com>
2024-03-03 17:31:11 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -273,7 +273,22 @@
 
 
 
-
+// Smooth scroll for mobile
+var smoothScroll = function() {
+	$('a[href*="#"]:not([href="#"])').on('click', function() {
+	  if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+		var target = $(this.hash);
+		target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+		if (target.length) {
+		  $('html, body').animate({
+			scrollTop: target.offset().top - 70
+		  }, 800);
+		  return false;
+		}
+	  }
+	});
+  };
+  
 
 })(jQuery);
 
